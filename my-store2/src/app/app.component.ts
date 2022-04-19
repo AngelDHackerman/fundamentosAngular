80162e997a5284e139f6a3af67cc7010cb3e59bb
angelDariaux <angeldariaux@gmail.com>
2022-04-18 19:52:27 -0600

    escuchando eventos en Angular

--- my-store2/src/app/app.component.ts
+++ my-store2/src/app/app.component.ts
@@ -11,10 +11,27 @@ export class AppComponent {
   public age = 26;
   public img = 'https://http.cat/100.jpg';
 
-  btnDisable = true;
+  btnDisabled = true;
   person = {
     name: 'Angel',
     age: 26,
     avatar: 'https://http.cat/506.jpg'
   }
+
+  toggleButton () {
+    this.btnDisabled = !this.btnDisabled; // ! Asi creamos un boton que se enciende y se apaga
+  }
+
+  ageIncrease() {
+    if (this.person.age < 100 ) {
+      this.person.age += 1;
+    }
+  }
+
+  ageDecrease () {
+    if (this.person.age > 0) {
+      this.person.age -= 1;
+    }
+  }
+
 }
